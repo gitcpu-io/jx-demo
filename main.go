20e3782c2ad500f99cf1acfa7ba52086984a5b56
朱大仙儿 <zhuhonglei@cloudtogo.cn>
2022-01-11 17:45:39 +0800

    init

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "net/http"
+  "time"
+)
+
+func main() {
+  http.HandleFunc("/", HelloServer)
+  fmt.Println("Server is Listening on 8081")
+  http.ListenAndServe(":8081", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello - Jenkins-X: %d", Random())
+}
+
+func Random() int {
+  s := rand.New(rand.NewSource(time.Now().Unix()))
+  return s.Intn(100)
+}
